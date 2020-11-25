c544f920c5c02c40ed5e87636765deacefa06c26
Bill Behrman <behrman@stanford.edu>
2020-11-24 16:51:43 -0800

    Initial commit

---
+++ _common.R
@@ -0,0 +1,16 @@
+set.seed(42)
+
+options(
+  digits = 3,
+  dplyr.print_max = 6,
+  dplyr.print_min = 6,
+  dplyr.summarise.inform = FALSE
+)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = FALSE,
+  fig.align = 'center',
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
+)
