3deb77b62bb04796b840f922487cb7c865a101db
Bill Behrman <behrman@stanford.edu>
2021-01-04 19:35:43 -0800

    Add predictive_intervals()

--- _common.R
+++ _common.R
@@ -42,6 +42,6 @@ predictive_intervals <- function(.data, fit, probs = c(0.5, 0.9)) {
 }
 
 # Sequence of evenly spaced points spanning the range of x
-seq_range <- function(x, n = 101) {
+seq_range <- function(x, n = 201) {
   seq(from = min(x, na.rm = TRUE), to = max(x, na.rm = TRUE), length.out = n)
 }
